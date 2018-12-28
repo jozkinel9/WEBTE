c045192ae2aa9b1147e7705e39964d72e0c06af3
shinthawan <45428884+shinthawan@users.noreply.github.com>
2018-12-28 17:02:26 +0100

    init menu horizontal

--- menu.js
+++ menu.js
@@ -2,6 +2,10 @@
 function createMenu() {
     var menu=["Hry", "O nás", "How to..", "Mapa", "Info"];
     for(i=0;i<menu.length;i++){
-        document.getElementById("menu").innerHTML+="<li>"+menu[i]+"</li>";
-        }
+        var li = document.createElement("li");
+        var textnode = document.createTextNode(menu[i]);
+        li.appendChild(textnode);
+        li.setAttribute("id", "li" + i);
+        document.getElementById("menu").appendChild(li);
+    }
 }
